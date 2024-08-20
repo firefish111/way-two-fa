863039ebab34f6cfa372a53bdae4ea89c06f859e
firefish <firefish111@protonmail.com>
2024-08-20 18:03:19 +0100

    Implemented peeking to next 30 seconds' code
    (ui code is gradually becoming more and more spaghetti)

--- src/ui.rs
+++ src/ui.rs
@@ -46,7 +46,8 @@ impl App {
   }
 
   pub fn handle_events(&mut self) -> io::Result<()> {
-    if event::poll(std::time::Duration::from_millis(16))? /* 16ms ~= 1 frame */ {
+    // check if it has been 25ms, to make it non blocking
+    if event::poll(std::time::Duration::from_millis(25))? /* 25ms = 40/s */ {
       match event::read()? {
         Event::Key(kev) if kev.kind == KeyEventKind::Press => {
           // manage keydown
@@ -55,14 +56,18 @@ impl App {
             KeyCode::Char('p') => self.is_peek = !self.is_peek,
             _ => {}
           }
-        }
+        },
         _ => {}
-      };
+      }
     }
 
     Ok(())
   }
 }
+
+const BORDER_WIDTH: u16 = 2_u16;
+const CODE_WIDTH: u16 = 9_u16;
+const PADDING: (u16, u16) = (4_u16, 1_u16); // padding
 
 // widgets are just lots of components, therefore the whole application is just a big widget
 // also has to be implemented for reference to it to once again placate borrow checker
@@ -79,15 +84,16 @@ impl Widget for &App {
       ]))
       .alignment(Alignment::Left);
 
+    // a bit lengthy
     let instrs = Title::from(Line::from(vec![
         " ".into(),
         " ".on_blue(),
         "Q".light_yellow().on_blue().bold().underlined(),
         "uit ".light_red().on_blue().bold(),
         " | ".into(),
-        " ".on_blue(),
-        "P".light_yellow().on_blue().bold().underlined(),
-        "eek ".light_red().on_blue().bold(),
+        " ".bg(if self.is_peek { Color::Cyan } else { Color::Blue }),
+        "P".light_yellow().bold().underlined().bg(if self.is_peek { Color::Cyan } else { Color::Blue }),
+        "eek ".light_red().bold().bg(if self.is_peek { Color::Cyan } else { Color::Blue }),
         " | ".into(),
         " ".on_blue(),
         "N".light_yellow().on_blue().bold().underlined(),
@@ -97,11 +103,20 @@ impl Widget for &App {
       .position(Position::Bottom)
       .alignment(Alignment::Center);
 
-    let blk = Block::bordered()
+    let mut blk = Block::bordered()
       .title(titl)
       .title(instrs)
       .padding(Padding::horizontal(1))
       .border_set(border::DOUBLE);
+
+    // ONPEEK
+    if self.is_peek {
+      blk = blk.title(Title::from(Line::from(vec![
+        " ".into(),
+        " Next Code ".light_cyan().on_yellow().bold(),
+        " ".into(),
+      ])).alignment(Alignment::Right));
+    }
 
     let mut para = Vec::new();
     let mut progs = Vec::new();
@@ -111,7 +126,7 @@ impl Widget for &App {
       // actual key
       let ky = ac.gen_key(time / ac.interv);
 
-      let topush = Line::from(vec![
+      let topush = vec![
         ac.name.clone().light_cyan().bold(),
         match ac.acc_id {
           Some(ref unm) => format!(" <@{}>", unm),
@@ -119,7 +134,21 @@ impl Widget for &App {
         }.light_blue(),
         ": ".into(),
         format!("{:0>3} {:0>3}", ky / 1_000, ky % 1000).bold(),
-      ]);
+      ];
+
+      // ONPEEK
+      if self.is_peek {
+        let peeky = ac.gen_key((time / ac.interv) + 1);
+        let peek_where = Rect {
+          x: area.width - CODE_WIDTH - PADDING.1 - BORDER_WIDTH, // +1 for padding
+          y: (para.len() + 1) as u16 + area.y,
+          width: CODE_WIDTH,
+          height: 1,
+        };
+        (format!(" {:0>3} {:0>3} ", peeky / 1_000, peeky % 1000).light_yellow().italic())
+          .render(peek_where, buf);
+      }
+      
 
       let prog = LineGauge::default()
         .filled_style(Style::default().fg(match secs {
@@ -131,24 +160,23 @@ impl Widget for &App {
             25..=30 => Color::LightRed,
             20..=24 => Color::Yellow,
             0..=19 => Color::LightGreen,
-            _ => Color::Cyan
+            _ => Color::Cyan,
           }).bold())
         .ratio((secs as f64) / (ac.interv as f64))
         .line_set(symbols::line::DOUBLE);
       
-      para.push(topush);
       progs.push(prog);
+      para.push(Line::from(topush));
     }
 
     // now row number
     let text_len = para.iter().cloned().map(|ln| String::from(ln).len() as u16).max().unwrap_or_default();
-    const BORDER_WIDTH: u16 = 2_u16;
-    const PADDING: (u16, u16) = (4_u16, 1_u16); // padding
+
     for (rn, gbar) in progs.iter().enumerate() {
       let progbar_where = Rect {
-        x: text_len + area.y + PADDING.0 + BORDER_WIDTH, // +1 for padding
-        y: (rn + 1) as u16 + area.x,
-        width: area.width - text_len - PADDING.0 - PADDING.1 - 2*BORDER_WIDTH,
+        x: text_len + area.x + PADDING.0 + BORDER_WIDTH, // +1 for padding
+        y: (rn + 1) as u16 + area.y,
+        width: area.width - text_len - PADDING.0 - PADDING.1 - 2*BORDER_WIDTH - if self.is_peek { CODE_WIDTH + 2 } else { 0 },
         height: 1,
       };
       gbar.render(progbar_where, buf);
